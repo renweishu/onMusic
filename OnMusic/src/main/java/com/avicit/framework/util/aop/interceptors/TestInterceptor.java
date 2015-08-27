d6c8ed5a5de54e7ff785e6e9116dc1d7f34a17ea
mac <mac@machao>
2015-08-27 20:13:53 +0800

    aop测试

---
+++ OnMusic/src/main/java/com/avicit/framework/util/aop/interceptors/TestInterceptor.java
@@ -0,0 +1,49 @@
+/*
+ * @(#)BLogicAcquireLockExceptionInterceptor.java
+ *
+ * Copyright(c) 2013 NTTDATA Corporation
+ *
+ * This software is the confidential and proprietary information
+ * of NTTData. ("Confidential Information").
+ */
+package com.avicit.framework.util.aop.interceptors;
+
+
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * [機 能]：排他制御
+ * [説 明]：データレコード例外が発生した場合、Validation例外とし、
+ *          排他エラーをセットする。
+ *
+ * @author  [新規作成] 2013/02/26 NDC@3HB2B 張軼胥
+ * @version $Revision$
+ */
+public class TestInterceptor implements MethodInterceptor {
+
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        // CHECKSTYLEON
+        Object result = null;
+        try {
+            result = invocation.proceed();
+            
+            System.out.println("======================================================");
+            System.out.println("==                                                  ==");
+            System.out.println("==AOP测试                                                                                                                                    ==");
+            System.out.println("==                                                  ==");
+            System.out.println("======================================================");
+            
+            // CHECKSTYLEOFF
+        } catch (Throwable e) {
+
+            throw e;
+        }
+        return result;
+    }
+}
